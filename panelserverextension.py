db9ee0acdc90d5e99d8e7c1b6fd4138c8c4a97d7
Nick Hand <nicholas.adam.hand@gmail.com>
2019-04-30 22:23:02 -0400

    use pandas for loading and edit max message size

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,15 @@
 from subprocess import Popen
 
+
 def load_jupyter_server_extension(nbapp):
     """serve the app.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "app.ipynb", "--allow-websocket-origin=*"])
+    Popen(
+        [
+            "panel",
+            "serve",
+            "app.ipynb",
+            "--allow-websocket-origin=*",
+            "--websocket-max-message-size=100",
+        ]
+    )
+
